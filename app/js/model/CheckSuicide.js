93dac2db8c31cfd70dd8736f48e88a4de7d33116
NovoseletskiyVadim <novoseletskiy.v@gmail.com>
2019-04-07 13:44:36 +0300

    G/D  Add class 'CheckSuiside.js'

--- app/js/model/CheckSuicide.js
+++ app/js/model/CheckSuicide.js
@@ -6,3 +6,55 @@
 
 */
 
+export default class CheckSuiside{
+
+    // the constructor accepts an object which returns the class "CheckBusyField"
+
+    constructor(checkBusyFieldObj){
+
+        this.territoryObject=checkBusyFieldObj;
+        this.stateTerritory=checkBusyFieldObj.stateTerritory;
+
+    };
+
+    checkSuiside(checkBusyFieldObj){
+
+        let result;
+
+        if(this.territoryObject!=null||this.territoryObject!=undefined){
+
+            let checkTerretory=this.stateTerritory;
+
+            if(checkTerretory=='black'){
+
+            }
+            else{
+
+            }
+
+
+        }
+        else{
+            let territoryObject=checkBusyFieldObj;
+            let stateTerritory=territoryObject.stateTerritory;
+
+            let checkTerretory=stateTerritory;
+
+            if(checkTerretory=='black'){
+                
+            }
+            else{
+
+            }
+             
+        }
+
+        return result;
+
+
+    }
+
+
+
+
+}
